9dee890f3fcddf486a76134edfd2dadca57860f7
KaneShaw <742834664@qq.com>
2017-05-30 23:13:58 +0800

    创建项目

---
+++ app/src/main/java/com/kane/neteasymusic/utils/GlobalConsts.java
@@ -0,0 +1,9 @@
+package com.kane.neteasymusic.utils;
+
+/**
+ * Created by Kane on 2017/5/30.
+ */
+
+public class GlobalConsts {
+    private String baseUrl = "https://www.showapi.com/api/lookPoint/213";
+}
